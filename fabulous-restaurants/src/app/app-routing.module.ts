177de0d248171c46565446b8c523c3abac36a8dc
You can call me J <jsy724724@gmail.com>
2022-04-06 00:14:45 -0400

    Update app-routing.module.ts

--- fabulous-restaurants/src/app/app-routing.module.ts
+++ fabulous-restaurants/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import {AccountModule} from "./account/account.module";
 import {AboutComponent} from "./about/about.component";
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const restaurantModule = () => import('./restaurant/restaurant.module').then(x => x.RestaurantModule);
+const reviewModule = () => import('./review/review.module').then(x => x.ReviewModule);
 
 const routes: Routes = [
     // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     // {path: 'users', loadChildren: usersModule, canActivate: [AuthGuard]},
     {path: "", component: HomeComponent},
     {path: "account", loadChildren: accountModule},
+    {path: "restaurant", loadChildren: restaurantModule},
+    {path: "review", loadChildren: reviewModule},
     {path: "about", component: AboutComponent},
     {path: '**', redirectTo: ''}
 ];
