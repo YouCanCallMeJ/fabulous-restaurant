8f3f6f62e50e87c76dbf898d142ec75840d8c1c2
You can call me J <jsy724724@gmail.com>
2022-04-06 00:14:33 -0400

    Update restaurant Routing

--- fabulous-restaurants/src/app/restaurant/restaurant-routing.module.ts
+++ fabulous-restaurants/src/app/restaurant/restaurant-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./layout/layout.component";
+import {ListComponent} from "./list/list.component";
+import {AddEditComponent} from "./add-edit/add-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "", component: LayoutComponent,
+        children: [
+            {path: "list", component: ListComponent},
+            {path: "add-edit", component: AddEditComponent}
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class RestaurantRoutingModule { }
+export class RestaurantRoutingModule {
+}
--- fabulous-restaurants/src/app/restaurant/restaurant.module.ts
+++ fabulous-restaurants/src/app/restaurant/restaurant.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RestaurantRoutingModule } from './restaurant-routing.module';
+import { LayoutComponent } from './layout/layout.component';
+import { ListComponent } from './list/list.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    ListComponent,
+    AddEditComponent
+  ],
   imports: [
     CommonModule,
     RestaurantRoutingModule
