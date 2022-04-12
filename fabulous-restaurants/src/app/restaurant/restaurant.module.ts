c99828ffa316cce4e8da60a9fdc548b653c998b0
You can call me J <jsy724724@gmail.com>
2022-04-12 11:50:32 -0400

    Update restaurant.module.ts

--- fabulous-restaurants/src/app/restaurant/restaurant.module.ts
+++ fabulous-restaurants/src/app/restaurant/restaurant.module.ts
@@ -5,6 +5,7 @@ import { RestaurantRoutingModule } from './restaurant-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { ListComponent } from './list/list.component';
 import { AddEditComponent } from './add-edit/add-edit.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -13,9 +14,10 @@ import { AddEditComponent } from './add-edit/add-edit.component';
     ListComponent,
     AddEditComponent
   ],
-  imports: [
-    CommonModule,
-    RestaurantRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        RestaurantRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class RestaurantModule { }
