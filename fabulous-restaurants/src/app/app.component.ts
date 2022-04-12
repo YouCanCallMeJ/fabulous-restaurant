946e82c68b04abbfe2ce99467b050a567d6f23d6
You can call me J <jsy724724@gmail.com>
2022-04-12 11:49:34 -0400

    Update app.component.ts

--- fabulous-restaurants/src/app/app.component.ts
+++ fabulous-restaurants/src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RestaurantDatabaseService} from "./services/restaurant-database.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fabulous-restaurants';
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  
-  logout() {
-  
-  }
+    title = 'fabulous-restaurants';
+    
+    constructor(private restaurantDatabase: RestaurantDatabaseService) {
+    }
+    
+    ngOnInit(): void {
+        this.restaurantDatabase.initDBRestaurant();
+    }
+    
+    logout() {
+    
+    }
 }
