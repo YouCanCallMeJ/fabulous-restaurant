9ed049d5845d989e26b6cfae2af1305a7fe5ead3
You can call me J <jsy724724@gmail.com>
2022-04-12 11:51:02 -0400

    Update list.component.ts

--- fabulous-restaurants/src/app/restaurant/list/list.component.ts
+++ fabulous-restaurants/src/app/restaurant/list/list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestaurantDALService} from "../../services/restaurant-dal.service";
+import {Restaurant} from "../../models/restaurant.model";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    restaurants: Restaurant[] = null;
+    
+    constructor(private restaurantDALService: RestaurantDALService) {
+    }
+    
+    ngOnInit(): void {
+        this.restaurantDALService.selectAllRestaurant()
+            .then(data => this.restaurants = data)
+            .catch(error => console.log(error));
+    }
+    
 }
