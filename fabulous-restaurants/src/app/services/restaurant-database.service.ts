1b0f4296ce2704961bfe90d4509ce04015c061a2
You can call me J <jsy724724@gmail.com>
2022-04-13 10:14:56 -0400

    Separate Restaurant DAL, Create new Tables

--- fabulous-restaurants/src/app/services/restaurant-database.service.ts
+++ fabulous-restaurants/src/app/services/restaurant-database.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Restaurant} from "../models/restaurant.model";
 
 declare function openDatabase(shortName, version, displayName, dbSize, dbCreateSuccess): any;
 
@@ -36,7 +35,27 @@ export class RestaurantDatabaseService {
                 "restaurantPhone TEXT NOT NULL);";
             let options = [];
             
-            tx.executeSql(sql, options, () => console.log("Success: create table successfully"), RestaurantDatabaseService.errorHandler);
+            tx.executeSql(sql, options, () => console.log("Success: create table restaurants successfully"), RestaurantDatabaseService.errorHandler);
+    
+            sql = "CREATE TABLE IF NOT EXISTS users(" +
+                "userId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "username TEXT NOT NULL," +
+                "password TEXT NOT NULL," +
+                "firstName TEXT NOT NULL," +
+                "lastName TEXT NOT NULL);";
+    
+            tx.executeSql(sql, options, () => console.log("Success: create table users successfully"), RestaurantDatabaseService.errorHandler);
+            
+            sql = "CREATE TABLE IF NOT EXISTS reviews(" +
+                "reviewId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "restaurantId INTEGER NOT NULL," +
+                "userId INTEGER NOT NULL," +
+                "reviewRating INTEGER NOT NULL," +
+                "reviewDetail TEXT NOT NULL," +
+                "FOREIGN KEY(restaurantId) REFERENCES restaurants(id)," +
+                "FOREIGN KEY(userId) REFERENCES users(userId));";
+
+            tx.executeSql(sql, options, () => console.log("Success: create table reviews successfully"), RestaurantDatabaseService.errorHandler);
         }
         
         this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: Table creation transaction successful"));
@@ -59,126 +78,6 @@ export class RestaurantDatabaseService {
         this.initDBRestaurant();
         return RestaurantDatabaseService.db;
     }
-    
-    // insertRestaurant(restaurant: Restaurant, callback) {
-    //     function txFunction(tx: any) {
-    //         let sql: string = "INSERT INTO restaurants" +
-    //             "(restaurantName, restaurantLocation, restaurantMainMenu, restaurantPhone) " +
-    //             "VALUES(?, ?, ?, ?);";
-    //         let options = [
-    //             restaurant.restaurantName,
-    //             restaurant.restaurantLocation,
-    //             restaurant.restaurantMainMenu,
-    //             restaurant.restaurantPhone
-    //         ];
-    //
-    //         tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
-    //     }
-    //
-    //     this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: insert transaction successfully"));
-    // }
-    //
-    // selectAllRestaurant(): Promise<any> {
-    //     let options = [];
-    //     let restaurants: Restaurant[] = [];
-    //
-    //     return new Promise((resolve, reject) => {
-    //         function txFunction(tx) {
-    //             let sql = "SELECT * FROM restaurants;";
-    //
-    //             tx.executeSql(sql, options, (tx, results) => {
-    //                 if (results.rows.length > 0) {
-    //                     for (let i = 0; i < results.rows.length; i++) {
-    //                         let row = results.rows[i];
-    //                         let restaurant = new Restaurant(
-    //                             row['restaurantName'],
-    //                             row['restaurantLocation'],
-    //                             row['restaurantMainMenu'],
-    //                             row['restaurantPhone']
-    //                         );
-    //                         restaurant.id = row['id'];
-    //                         restaurants.push(restaurant);
-    //                     }
-    //                     resolve(restaurants);
-    //                 }
-    //                 // reject
-    //                 else {
-    //                     reject("No books found");
-    //                 }
-    //             }, RestaurantDatabaseService.errorHandler)
-    //         }
-    //
-    //         this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: selectAll transaction successfully"));
-    //
-    //     });
-    // }
-    //
-    // deleteRestaurant(restaurant: Restaurant, callback) {
-    //     function txFunction(tx: any) {
-    //         let sql: string = "DELETE FROM restaurants WHERE id=?;";
-    //         let options = [restaurant.id];
-    //         tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
-    //     }
-    //
-    //     this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: delete transaction successfully"));
-    // }
-    //
-    // updateRestaurant(restaurant: Restaurant, callback) {
-    //     function txFunction(tx: any) {
-    //         let sql: string = "UPDATE restaurants SET " +
-    //             "restaurantName=?, " +
-    //             "restaurantLocation=?, " +
-    //             "restaurantMainMenu=?, " +
-    //             "restaurantPhone=?, " +
-    //             "WHERE id=?;";
-    //
-    //         // options parameter order does matter here!
-    //         let options = [
-    //             restaurant.restaurantName,
-    //             restaurant.restaurantLocation,
-    //             restaurant.restaurantMainMenu,
-    //             restaurant.restaurantPhone,
-    //             restaurant.id
-    //         ];
-    //
-    //         tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
-    //     }
-    //
-    //     this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: update transaction successfully"));
-    // }
-    //
-    // selectRestaurant(id: number): Promise<any> {
-    //     let options = [id];
-    //
-    //     let restaurant: Restaurant = null;
-    //
-    //     return new Promise((resolve, reject) => {
-    //         function txFunction(tx) {
-    //             let sql = "SELECT * FROM restaurants WHERE id=?;";
-    //
-    //             tx.executeSql(sql, options, (tx, results) => {
-    //                 // resolve
-    //                 if (results.rows.length > 0) {
-    //                     let row = results.rows[0];
-    //                     let restaurant = new Restaurant(
-    //                         row['restaurantName'],
-    //                         row['restaurantLocation'],
-    //                         row['restaurantMainMenu'],
-    //                         row['restaurantPhone']
-    //                     );
-    //                     restaurant.id = row['id'];
-    //                     resolve(restaurant);
-    //                 }
-    //                 // reject
-    //                 else {
-    //                     reject("No such restaurant found");
-    //                 }
-    //             }, RestaurantDatabaseService.errorHandler)
-    //         }
-    //
-    //         this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: select transaction successfully"));
-    //     });
-    // }
     
     private dropTablesRestaurant(): void {
         function txFunction(tx: any): void {
