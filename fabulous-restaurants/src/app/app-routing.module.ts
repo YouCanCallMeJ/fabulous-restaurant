00b4e5d17225a29cb98210b0be1c1ec1efdb00e1
You can call me J <jsy724724@gmail.com>
2022-04-13 18:06:30 -0400

    Update app-routing.module.ts

--- fabulous-restaurants/src/app/app-routing.module.ts
+++ fabulous-restaurants/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {AuthenticationGuard} from "./authentication.guard";
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const userModule = () => import('./user/user.module').then(x => x.UserModule);
@@ -9,12 +10,14 @@ const restaurantModule = () => import('./restaurant/restaurant.module').then(x =
 const reviewModule = () => import('./review/review.module').then(x => x.ReviewModule);
 
 const routes: Routes = [
-    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    // {path: 'users', loadChildren: usersModule, canActivate: [AuthGuard]},
+    // {path: "", component: HomeComponent, canActivate: [AuthenticationGuard]},
     {path: "", component: HomeComponent},
     {path: "account", loadChildren: accountModule},
+    // {path: "user", loadChildren: userModule, canActivate: [AuthenticationGuard]},
     {path: "user", loadChildren: userModule},
+    // {path: "restaurant", loadChildren: restaurantModule, canActivate: [AuthenticationGuard]},
     {path: "restaurant", loadChildren: restaurantModule},
+    // {path: "review", loadChildren: reviewModule, canActivate: [AuthenticationGuard]},
     {path: "review", loadChildren: reviewModule},
     {path: "about", component: AboutComponent},
     {path: "**", redirectTo: ""}
