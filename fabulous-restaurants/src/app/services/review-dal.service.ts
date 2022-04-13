c52700bb7a4aebd49ce01393f0417d9abf99aaff
You can call me J <jsy724724@gmail.com>
2022-04-13 13:59:42 -0400

    Update review-dal.service.ts

--- fabulous-restaurants/src/app/services/review-dal.service.ts
+++ fabulous-restaurants/src/app/services/review-dal.service.ts
@@ -77,4 +77,62 @@ export class ReviewDALService {
         
         this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: delete review transaction successfully"));
     }
+    
+    selectReview(reviewId: number): Promise<any> {
+        let options = [reviewId];
+        
+        let review: Review = null;
+        
+        return new Promise((resolve, reject) => {
+            function txFunction(tx) {
+                let sql = "SELECT * FROM reviews WHERE reviewId=?;";
+                
+                tx.executeSql(sql, options, (tx, results) => {
+                    // resolve
+                    if (results.rows.length > 0) {
+                        let row = results.rows[0];
+                        let review = new Review(
+                            row['restaurantId'],
+                            row['userId'],
+                            row['reviewRating'],
+                            row['reviewDetail']
+                        );
+                        review.reviewId = row['reviewId'];
+                        resolve(review);
+                    }
+                    // reject
+                    else {
+                        reject("No such review found");
+                    }
+                }, RestaurantDatabaseService.errorHandler)
+            }
+            
+            this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: select review transaction successfully"));
+        });
+    }
+    
+    updateReview(review: Review, callback) {
+        function txFunction(tx: any) {
+            let sql: string = "UPDATE reviews SET " +
+                "restaurantId=?, " +
+                "userId=?, " +
+                "reviewRating=?, " +
+                "reviewDetail=? " +
+                "WHERE reviewId=?;";
+            
+            // options parameter order does matter here!
+            let options = [
+                review.restaurantId,
+                review.userId,
+                review.reviewRating,
+                review.reviewDetail,
+                review.reviewId
+            ];
+            
+            console.log(options);
+            tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
+        }
+        
+        this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: update review transaction successfully"));
+    }
 }
