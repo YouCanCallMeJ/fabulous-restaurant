7948002ca26f7dbdabf7eeec2ec66719ebcd1372
You can call me J <jsy724724@gmail.com>
2022-04-13 13:41:30 -0400

    Update list.component.ts

--- fabulous-restaurants/src/app/review/list/list.component.ts
+++ fabulous-restaurants/src/app/review/list/list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ReviewDALService} from "../../services/review-dal.service";
+import {Review} from "../../models/review.model";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  reviews: Review[] = null;
+  
+    constructor(private reviewDALService: ReviewDALService) {
+    }
+    
+    ngOnInit(): void {
+      this.reviewDALService.selectAllReview()
+          .then(data => this.reviews = data)
+          .catch(error => console.log(error));
+    }
+  
+  deleteReview(reviewId: number) {
+      const review = this.reviews.find(x => x.reviewId === reviewId);
+      this.reviewDALService.deleteReview(review, () => {
+          alert("Record review deleted successfully");
+      });
+      this.ngOnInit();
   }
-
 }
