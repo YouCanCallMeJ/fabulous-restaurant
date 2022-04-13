7cd3368136805e6f373547b8554e9ee346432405
You can call me J <jsy724724@gmail.com>
2022-04-13 11:27:42 -0400

    Update account.service.ts

--- fabulous-restaurants/src/app/services/account.service.ts
+++ fabulous-restaurants/src/app/services/account.service.ts
@@ -63,4 +63,68 @@ export class AccountService {
             this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: selectAllUser transaction successfully"));
         });
     }
+    
+    deleteUser(user: User, callback) {
+        function txFunction(tx: any) {
+            let sql: string = "DELETE FROM users WHERE userId=?;";
+            let options = [user.userId];
+            
+            console.log(options);
+            tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
+        }
+        
+        this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: delete user transaction successfully"));
+    }
+    
+    selectUser(userId: number): Promise<any> {
+        let options = [userId];
+        
+        let user: User = null;
+        
+        return new Promise((resolve, reject) => {
+            function txFunction(tx) {
+                let sql = "SELECT * FROM users WHERE userId=?;";
+                
+                tx.executeSql(sql, options, (tx, results) => {
+                    // resolve
+                    if (results.rows.length > 0) {
+                        let row = results.rows[0];
+                        let user = new User(
+                            row['username'],
+                            row['password'],
+                            row['firstName'],
+                            row['lastName']
+                        );
+                        user.userId = row['userId'];
+                        resolve(user);
+                    }
+                    // reject
+                    else {
+                        reject("No such user found");
+                    }
+                }, RestaurantDatabaseService.errorHandler)
+            }
+            
+            this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: select user transaction successfully"));
+        });
+    }
+    
+    updateUser(user: User, callback) {
+        function txFunction(tx: any) {
+            let sql: string = "UPDATE users SET username=?, password=?, lastName=?, firstName=? WHERE userId=?;";
+            
+            // options parameter order does matter here!
+            let options = [
+                user.username,
+                user.password,
+                user.firstName,
+                user.lastName,
+                user.userId
+            ];
+            
+            tx.executeSql(sql, options, callback, RestaurantDatabaseService.errorHandler);
+        }
+        
+        this.getDatabaseRestaurant().transaction(txFunction, RestaurantDatabaseService.errorHandler, () => console.log("Success: update user transaction successfully"));
+    }
 }
