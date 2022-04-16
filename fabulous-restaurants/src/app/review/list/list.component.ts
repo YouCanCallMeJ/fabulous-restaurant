dce2884a05bfbc2512ca3bb5822a979067abf5a6
You can call me J <jsy724724@gmail.com>
2022-04-16 14:43:40 -0400

    Show Restaurant Name instead of Restaurant Id

--- fabulous-restaurants/src/app/review/list/list.component.ts
+++ fabulous-restaurants/src/app/review/list/list.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ReviewDALService} from "../../services/review-dal.service";
 import {Review} from "../../models/review.model";
+import {Router} from "@angular/router";
+import {Restaurant} from "../../models/restaurant.model";
+import {RestaurantDALService} from "../../services/restaurant-dal.service";
 
 @Component({
     selector: 'app-list',
@@ -9,13 +12,25 @@ import {Review} from "../../models/review.model";
 })
 export class ListComponent implements OnInit {
     reviews: Review[] = null;
+    restaurants: Restaurant[] = null;
     
-    constructor(private reviewDALService: ReviewDALService) {
+    constructor(
+        private router: Router,
+        private reviewDALService: ReviewDALService,
+        private restaurantDALService: RestaurantDALService) {
     }
     
     ngOnInit(): void {
         this.reviewDALService.selectAllReview()
-            .then(data => this.reviews = data)
+            .then(data => {
+                this.reviews = data;
+            })
+            .catch(error => console.log(error));
+        this.restaurantDALService.selectAllRestaurant()
+            .then(data => {
+                this.restaurants = data;
+                console.log(this.restaurants[0].restaurantName);
+            })
             .catch(error => console.log(error));
     }
     
