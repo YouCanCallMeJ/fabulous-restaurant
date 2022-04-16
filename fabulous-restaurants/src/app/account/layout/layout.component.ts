e75c2f2cda7b34264de31f69fa0d76f5cd211202
You can call me J <jsy724724@gmail.com>
2022-04-16 10:13:15 -0400

    Redirect to home if already logged in

--- fabulous-restaurants/src/app/account/layout/layout.component.ts
+++ fabulous-restaurants/src/app/account/layout/layout.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AccountService} from "../../services/account.service";
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+    selector: 'app-layout',
+    templateUrl: './layout.component.html',
+    styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    
+    constructor(
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.userValue) {
+            this.router.navigate(['/']);
+        }
+    }
+    
+    ngOnInit(): void {
+    }
+    
 }
